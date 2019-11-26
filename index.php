3354bf6f29eb72309df48f998a09c2443e6ad7b4
sunyu90325 <54927055+sunyu90325@users.noreply.github.com>
2019-11-25 23:30:08 -0700

    Revert "Frame and navigation bar"
    This reverts commit 7af536559c9b2677f66b84e976e1a8f991d07441.

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html>
 <head>
 	<title>Miner</title>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="header.css" />
 </head>
 <body>
-	<?php 
-	  $path = "";
-	  $thisPage = "home";
-	  include 'templateHeader.php';
+	<?php
+		include('templateHeader.php');
 	?>
-
-	
 </body>
 </html>
