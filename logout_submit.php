49e35d5d946dca68d9e92bd0080b85b0dd13ceb6
sunyu90325 <54927055+sunyu90325@users.noreply.github.com>
2019-12-03 16:26:46 -0700

    add change password page and logout page

---
+++ logout_submit.php
@@ -0,0 +1,24 @@
+<?php
+include 'connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Miner</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="header.css" />
+</head>
+<body>
+	<?php 
+	  $path = "";
+	  $thisPage = "logout";
+	  include 'templateheader.php';
+	?>
+		
+	<!-- Body of Web Page -->
+	<main>
+		<h2>You have successfully logged out!</h2>
+	</main>
+</body>
+</html>
